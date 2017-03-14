3ec3f2d71f5add910d034a8fd03ad8b578aa1ec2
Thomas Mayrhofer <thomas@mayrhofer.at>
2017-03-14 21:50:26 +0100

    Random dump.

--- index.ios.js
+++ index.ios.js
@@ -11,7 +11,8 @@ import {
   Text,
   View,
   Image,
-  ListView
+  ListView,
+  TouchableOpacity
 } from 'react-native';
 
 
@@ -32,18 +33,80 @@ const headingStyles = StyleSheet.create({
 
 
 const myData = [
-  { name: "Coffee 1", address: "Munich" },
-  { name: "Coffee 2", address: "Tamil Nadu" },
-  { name: "Coffee 3", address: "Linz" },
+  { id: 1, name: "Coffee 1", address: "Munich" },
+  { id: 2, name: "Coffee 2", address: "Tamil Nadu" },
+  { id: 3, name: "Coffee 3", address: "Linz" },
 ];
 
+
+
+const ListViewItem = (attrs) => {
+  const coffeePlace = attrs.coffeePlace;
+
+ return (
+   <TouchableOpacity onPress={ () => attrs.onPress(coffeePlace.id) }>
+     <View style={styles.listItem}>
+       <View>
+         <Text>
+           { coffeePlace.name }
+         </Text>
+         <Text>
+           { coffeePlace.address }
+         </Text>
+       </View>
+       <Text>
+         Image
+       </Text>
+     </View>
+   </TouchableOpacity>
+ )
+};
+
+const CoffeeShopDetail = (props) => {
+  return (
+    <View>
+      <Text>
+        { props.coffeePlace.name }
+      </Text>
+
+      <TouchableOpacity onPress={ props.onExit }>
+        <Text>
+          Exit
+        </Text>
+      </TouchableOpacity>
+    </View>
+  )
+};
+
 export default class Playground extends Component {
+  constructor(props) {
+    super(props);
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      currentCoffeePlace: void 0,
+      dataSource: ds.cloneWithRows(myData),
+    };
+  }
+
+  onCoffeePlaceClicked = (id) => {
+    this.setState({ currentCoffeePlace: id })
+  };
+
+  onCoffeePlaceExit = () => {
+    this.setState({ currentCoffeePlace: void 0 })
+  };
+
   render() {
+    if(this.state.currentCoffeePlace !== void 0) {
+      const coffeePlace = myData.find((place) => place.id === this.state.currentCoffeePlace)
+
+      return (<CoffeeShopDetail coffeePlace={ coffeePlace } onExit={ this.onCoffeePlaceExit } />)
+    }
+
     return (
       <View style={styles.container}>
-        <ListView dataSource={ myData } renderRow={
-          (coffeePlace) => <Text>{ coffeePlace.name }</Text>
-        } />
+        <ListView dataSource={ this.state.dataSource } renderRow={ (coffeePlace) =>
+          <ListViewItem coffeePlace={ coffeePlace } onPress={ this.onCoffeePlaceClicked }/> } />
       </View>
     );
   }
@@ -51,10 +114,18 @@ export default class Playground extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    paddingTop: 50,
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
+
+  },
+  listItem: {
+    alignItems: 'center',
+    flex: 1,
+    padding: 5,
+    justifyContent: 'space-between',
+    flexDirection: 'row'
   },
   welcome: {
     fontSize: 20,
